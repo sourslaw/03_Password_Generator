a02d713b089b4940bdbd20acb8314388f73abbb2
sourslaw <lawanscott@gmail.com>
2021-04-06 21:54:07 -0700

    adding basic logic

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+const base = 'abcdefghijklmnopqrstuvwxyz';
+const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const numbers = '0123456789';
+const symbols = '!@#$%^&*=-_';
+
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+const generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
@@ -12,3 +17,19 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+length = 7;
+
+
+generate.addEventListener("click", function(event) {
+    event.preventDefault();
+    
+    let password = '';
+
+    for (let i = 0; i<length; i++) {
+        password += base.charAt(Math.floor(Math.random() * base.length));
+    }
+    console.log(password);
+    const passwordOut = document.getElementById("password");
+    passwordOut.value = password;
+});
