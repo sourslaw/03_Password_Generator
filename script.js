c723ad58f0bbf670f7269c6f72936fe2bd9c1e00
sourslaw <lawanscott@gmail.com>
2021-04-06 22:34:54 -0700

    working on logic for symbols, uppercase, numbers

--- script.js
+++ script.js
@@ -1,25 +1,35 @@
-const base = 'abcdefghijklmnopqrstuvwxyz';
+let base = 'abcdefghijklmnopqrstuvwxyz';
 const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const numbers = '0123456789';
 const symbols = '!@#$%^&*=-_';
 
 // Assignment Code
-const generateBtn = document.querySelector("#generate");
+// const generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+//   passwordText.value = password;
 
-}
+// }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// generateBtn.addEventListener("click", writePassword);
 
-length = 7;
+length = 19;
 
+const wannaSymbols = 1;
+const Wnumbers = 1;
+const Wupper = 1;
+
+if (wannaSymbols == 1) {
+  base = base.concat(symbols); 
+  
+}
+
+console.log(base);
 
 generate.addEventListener("click", function(event) {
     event.preventDefault();
@@ -29,7 +39,11 @@ generate.addEventListener("click", function(event) {
     for (let i = 0; i<length; i++) {
         password += base.charAt(Math.floor(Math.random() * base.length));
     }
+
     console.log(password);
+
+
+    // writes to the textarea
     const passwordOut = document.getElementById("password");
     passwordOut.value = password;
 });
