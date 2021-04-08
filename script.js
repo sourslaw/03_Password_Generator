132cce93ed6128842d69ebf7364d77d6ab77e405
sourslaw <lawanscott@gmail.com>
2021-04-08 14:48:06 -0700

    updating options logic

--- script.js
+++ script.js
@@ -20,23 +20,30 @@ const symbols = '!@#$%^&*=-_';
 
 // prompt crap
 
-// const incoming = promp()
 
-length = 19;
+let lengthOption = document.getElementById("lengthOption");
 
 
 
 // choice logic
-const wannaSymbols = 1;
-const Wnumbers = 1;
-const Wupper = 1;
+const symbolCheck = document.getElementById("symbolCheck");
+const numberCheck = document.getElementById("numberCheck");
+const upperCheck = document.getElementById("upperCheck");
 
-if (wannaSymbols == 1) {
-  base = base.concat(symbols); 
-  
-}
+// if (wannaSymbols == 1) {
+//   base = base.concat(symbols); 
+// }
 
-console.log(base);
+let save = document.getElementById("save");
+save.addEventListener("click", function() {
+  (symbolCheck.checked) ? base = base.concat(symbols): base;
+  (numberCheck.checked) ? base = base.concat(numbers): base;
+  (upperCheck.checked) ? base = base.concat(upperCase): base;
+  // return(base);
+
+})
+
+// console.log(base);
 
 // password generate
 generate.addEventListener("click", function(event) {
@@ -44,25 +51,18 @@ generate.addEventListener("click", function(event) {
     
     let password = '';
 
-    for (let i = 0; i<length; i++) {
+    for (let i = 0; i<lengthOption.value; i++) {
         password += base.charAt(Math.floor(Math.random() * base.length));
     }
 
     console.log(password);
-
 
     // writes to the textarea
     const passwordOut = document.getElementById("magichole");
     passwordOut.value = password;
 });
 
-
-// const options = document.querySelector("#options");
-
-// var myoptions = new bootstrap.Modal(document.getElementById('options'), {
-//   keyboard: false
-// })
-
-// myoptions.addEventListener("click", function() {
-//   console.log("hello");
-// })
+const close = document.getElementById("close");
+close.addEventListener("click", function() {
+  console.log(lengthOption.value);
+})
