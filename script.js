31bd4e5c1790d0cac593d66b3a8b85db7c10d5b5
sourslaw <lawanscott@gmail.com>
2021-04-08 15:38:23 -0700

    updating modal containers

--- script.js
+++ script.js
@@ -1,68 +1,42 @@
-let base = 'abcdefghijklmnopqrstuvwxyz';
 const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const numbers = '0123456789';
 const symbols = '!@#$%^&*=-_';
 
-// Assignment Code
-// const generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-
-// }
-
-// Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
-
-// prompt crap
-
-
+// number length
 let lengthOption = document.getElementById("lengthOption");
-
-
-
-// choice logic
+// complexity options
 const symbolCheck = document.getElementById("symbolCheck");
 const numberCheck = document.getElementById("numberCheck");
 const upperCheck = document.getElementById("upperCheck");
 
-// if (wannaSymbols == 1) {
-//   base = base.concat(symbols); 
-// }
-
-let save = document.getElementById("save");
-save.addEventListener("click", function() {
-  (symbolCheck.checked) ? base = base.concat(symbols): base;
-  (numberCheck.checked) ? base = base.concat(numbers): base;
-  (upperCheck.checked) ? base = base.concat(upperCase): base;
-  // return(base);
-
-})
-
-// console.log(base);
-
-// password generate
-generate.addEventListener("click", function(event) {
-    event.preventDefault();
-    
-    let password = '';
-
-    for (let i = 0; i<lengthOption.value; i++) {
-        password += base.charAt(Math.floor(Math.random() * base.length));
-    }
-
-    console.log(password);
-
-    // writes to the textarea
-    const passwordOut = document.getElementById("magichole");
-    passwordOut.value = password;
+// password generate (one big function)
+generate.addEventListener("click", function() {
+  // default base character string
+  let base = 'abcdefghijklmnopqrstuvwxyz';
+  // ternary operators
+  (symbolCheck.checked) ? base += symbols: '';
+  (numberCheck.checked) ? base += numbers: '';
+  (upperCheck.checked) ? base += upperCase: '';
+  // password initially set to empty string
+  let password = '';
+  // password creation happens here bro
+  for (let i = 0; i<lengthOption.value; i++) {
+      password += base.charAt(Math.floor(Math.random() * base.length));
+  }
+  // writes to the textarea
+  const passwordOut = document.getElementById("magichole");
+  passwordOut.value = password;
 });
 
-const close = document.getElementById("close");
-close.addEventListener("click", function() {
-  console.log(lengthOption.value);
-})
+
+
+// Assignment Code
+// const generateBtn = document.querySelector("#generate");
+// Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+//   passwordText.value = password;
+// }
+// Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
