da28c26b7f3a07402011442f3459509744492f51
sourslaw <lawanscott@gmail.com>
2021-04-08 20:56:14 -0700

    commenting script.js for clarity

--- assets/script.js
+++ assets/script.js
@@ -1,28 +1,28 @@
+// character strings
 const lowerCase = 'abcdefghijklmnopqrstuvwxyz';
 const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const numbers = '0123456789';
 const symbols = '!@#$%^&*=-_';
-
-// number length
+// length of generated password
 let lengthOption = document.getElementById("lengthOption");
 // complexity options
 const symbolCheck = document.getElementById("symbolCheck");
 const numberCheck = document.getElementById("numberCheck");
 const upperCheck = document.getElementById("upperCheck");
 const lowerCheck = document.getElementById("lowerCheck");
-// password
+// password for textarea
 const passwordOut = document.getElementById("magichole");
 
-// password generate (one big function)
+// password generator logic (one big function)
 generate.addEventListener("click", function() {
-        // default base character string
+    // default base character string (string password is built on/in)
     let base = '';
-    // ternary operators
+    // ternary operators for complexity options (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_Operator). if checked, added to the base . . .
     (symbolCheck.checked) ? base += symbols: '';
     (numberCheck.checked) ? base += numbers: '';
     (upperCheck.checked) ? base += upperCase: '';
     (lowerCheck.checked) ? base += lowerCase: '';
-    // password initially set to empty string
+    // password initially set to empty string to be written to the textarea
     let password = '';
     // password creation happens here bro
     for (let i = 0; i<lengthOption.value; i++) {
@@ -37,7 +37,7 @@ generate.addEventListener("click", function() {
     }
 });
 
-// clears selections and clears textarea
+// clears selections, resets password length to default (9), and clears textarea
 reset.addEventListener("click", function() {
     const inputs = document.querySelectorAll(".form-check-input");
     for (var i = 0; i < inputs.length; i++) {
